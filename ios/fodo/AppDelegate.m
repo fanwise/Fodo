35a1175461054222a7ec47e7c780bf9bedc7dca9
Wei Fan <weifan@Fanweis-MacBook-Pro.local>
2017-09-18 16:32:11 +0800

    fix nw_connection console problem, add init property for statusBarHeight in iOS native code

--- ios/fodo/AppDelegate.m
+++ ios/fodo/AppDelegate.m
@@ -12,26 +12,42 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@interface AppDelegate()
+
+@property(nonatomic, strong) RCTRootView *rootView;
+
+@end
+
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+  NSDictionary *props = @{@"statusBarHeight": [self currentStatusBarHeight]};
+                           
+  self.rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"fodo"
-                                               initialProperties:nil
+                                               initialProperties:props
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  self.rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
+  rootViewController.view = self.rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+                           
+- (void)application:(UIApplication *)application didChangeStatusBarFrame:(CGRect)oldStatusBarFrame {
+  self.rootView.appProperties = @{@"statusBarHeight": [self currentStatusBarHeight]};
+}
+
+- (NSNumber *)currentStatusBarHeight {
+  CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+  return [NSNumber numberWithFloat:statusBarHeight];
 }
 
 @end
